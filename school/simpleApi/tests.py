e44ff0199772837295589432c4a39b0cb8370671
Kiarash <kiarasha@alum.mit.edu>
2021-07-07 17:20:21 -0400

    Updated models

--- school/simpleApi/tests.py
+++ school/simpleApi/tests.py
@@ -4,8 +4,15 @@ from simpleApi.models import Teacher, Student, Gradeable
 
 class TeacherTestCase(TestCase):
     def setUp(self):
+        s = Student.objects.create(username="student1", password="student", first_name="student")
         Teacher.objects.create(username="teacher1", password="teacher", first_name="teacher")
-        Student.objects.create(username="student1", password="student", first_name="student")
+
+        g = Gradeable(name="homework1", student=s)
+        g.document = SimpleUploadedFile(
+            "best_file_eva.txt",
+            b"these are the file contents!"
+        )
+        g.save()
 
     def test_teacher_is_setup(self):
         teacher = Teacher.objects.get(username="teacher1")
@@ -15,7 +22,7 @@ class TeacherTestCase(TestCase):
         student = Student.objects.get(username="student1")
         self.assertEqual(student.get_full_name(), "student")
     
-    def test_create_gradeable(self):
+    def test_student_create_gradeable(self):
         
         g = Gradeable(name="homework1")
         g.student = Student.objects.get(username="student1")
@@ -25,3 +32,10 @@ class TeacherTestCase(TestCase):
         )
         g.save()
         self.assertEqual(g.grade, "ungraded")
+    
+    def test_student_get_grades(self):
+        s = Student.objects.get(username="student1")
+        
+        g = Gradeable.objects.filter(student_id=s.id)
+
+        self.assertEqual(len(g), 1)
